9e04a417283bc3f81643df7065b18f54810f0349
bbynocode <dlemm@csuchico.edu>
2024-03-04 06:41:25 -0800

    setup static files and created homepage

--- TCG_Binder/urls.py
+++ TCG_Binder/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 from core import views
 
 urlpatterns = [
+    path('', views.home, name="home"),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('accounts/profile/' , views.profile, name="profile" )
--- core/views.py
+++ core/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def profile(request):
-    return render(request, 'profile.html')
+    return render(request, 'users/profile.html')
+
+def home(request):
+    return render(request, 'main.html')
