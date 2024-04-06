55b62565123e5dcce1413b1b0d85123dfa2712e7
PepperJam-Loser <xc4ever1@gmail.com>
2024-04-05 23:35:02 -0700

    Rough Deck based of andre

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from .models import PageNumber  # Import the PageNumber model
+from django.db.models import Prefetch
+from .models import PageNumber, Deck, Card  # Import the PageNumber model
+from .forms import DeckForm, CardForm
 
 # Create your views here.
 
@@ -10,9 +13,42 @@ def profile(request):
 def home(request):
     return render(request, 'pages/homepage.html')
 
+@login_required
 def decks(request):
-    return render(request, 'pages/decks.html')
+    if request.method == 'POST':
+        deckFormIN = DeckForm(request.POST)
+        cardFormIN = CardForm(request.POST)
+        if deckFormIN.is_valid():
+            deck_name = deckFormIN.cleaned_data['deck_name']
+            user = request.user
+            deck = Deck.objects.create(user=user, deckName=deck_name)
+            return redirect('decks')  # Redirect to the same page after adding the deck
+        if cardFormIN.is_valid():
+            card_name = cardFormIN.cleaned_data['card_name']
+            deck_nameCard = cardFormIN.cleaned_data['deck_nameCard']
+            # Check if the card already exists in the database
+            existing_card = Card.objects.filter(cardName=card_name).first()
+            existing_deck = Deck.objects.get(deckName=deck_nameCard)
+            if existing_card:
+                pass
+            else:
+                existing_card = Card.objects.create(cardName=card_name)
+            existing_deck.cards.add(existing_card)
+            return redirect('decks')  # Redirect to the same page after adding the deck
+    else:
+        deckFormIN = DeckForm()
+        cardFormIN = CardForm()
+    decks = Deck.objects.filter(user=request.user).prefetch_related('cards')
+    return render(request, 'pages/decks.html', {'form': deckFormIN, 'form':cardFormIN, 'decks': decks})
 
+@login_required
+def delete_deck(request, deck_id):
+    deck = get_object_or_404(Deck, id=deck_id)
+    if request.method == 'POST':
+        deck.delete()
+    return redirect('decks')
+
+@login_required
 def addcard(request):
     return render(request, 'pages/addcard.html')
 
@@ -25,7 +61,6 @@ def library(request):
     if page_number_object:
         current_page = page_number_object.page
     return render(request, 'pages/library.html', {'current_page': current_page})
-
 
 def update_page_number(request):
     if request.method == 'POST':
